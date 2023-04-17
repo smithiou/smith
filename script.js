5625946214daf021099c8f317c3f20e893867ea7
smithiou <130704371+smithiou@users.noreply.github.com>
2023-04-17 14:23:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('form');
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  
+  // TODO: Implement login logic here
+  
+  console.log(`Username: ${username}, Password: ${password}`);
+});
